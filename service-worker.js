b5e7bf837b48df51ee99b6b71db5f33df12419ca
jimmmbo1989 <91218489+jimmmbo1989@users.noreply.github.com>
2025-10-16 22:02:52 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+// Simple service worker for offline caching
+const CACHE_NAME = 'gcl-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './forms.html',
+  './risk-assessment.html',
+  './manifest.json',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/logo.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      // Optionally cache new GET requests
+      if (req.method === 'GET' && res.status === 200 && res.type === 'basic') {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, resClone));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
